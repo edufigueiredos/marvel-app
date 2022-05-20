bd23bde7ff6d990fd2ae8781ac0f667f447d8432
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-05-20 18:48:53 -0300

    Criando a tela de início.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(module => module.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
